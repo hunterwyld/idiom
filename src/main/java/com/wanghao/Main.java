88c89ba5cf4f01255aef8f7b439e4dbbd91e4742
wanghao7 <2558157640@qq.com>
2020-05-01 10:52:01 +0800

    Initial commit

---
+++ src/main/java/com/wanghao/Main.java
@@ -0,0 +1,16 @@
+package com.wanghao;
+
+import com.wanghao.game.GameFlow;
+
+/**
+ * @author wanghao
+ * @description
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameFlow gameFlow = new GameFlow();
+        gameFlow.startGame();
+    }
+}
